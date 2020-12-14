17e19c94c5cf2814e54ff3ee11e9e50564177494
Vagaro <pajaromen@gmail.com>
2020-12-14 14:31:30 -0500

    chore: Added basic layout with zea-web-components

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,11 +1,22 @@
+<style>
+ .slot-container {
+   height: calc(100% - 2px);
+ }
+</style>
+
 <script>
   import { metatags } from '@roxi/routify'
+  import Header from '../components/Header.svelte'
+  // import { Scene } from '@zeainc/zea-engine'
 
   metatags.title = 'Zea Template Collab'
   metatags.description = 'Description coming soon...'
 </script>
 
-<div
-  class="bg-background flex h-screen justify-center items-center text-foreground">
-  <h1>Zea Template Collab</h1>
-</div>
+<zea-layout orientation="vertical" cell-a-size="50" resize-cell-a="false" cell-b-size="100%" cell-c-size="0px" resize-cell-c="false">
+  <div class="w-full slot-container" slot="a"><Header /></div>
+  <zea-layout slot="b" cell-a-size="300px" cell-b-size="100%" cell-c-size="0px" resize-cell-c="false" >
+    <div class="slot-container" slot="a">Sidebar </div>
+    <div class="slot-container" slot="b">Renderer</div>
+  </zea-layout>
+</zea-layout>
