a265c414586ff97e8cf010b8852391c5aa9a5633
Vagaro <pajaromen@gmail.com>
2020-12-14 14:57:35 -0500

    chore: Added

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -7,7 +7,6 @@
 <script>
   import { metatags } from '@roxi/routify'
   import Header from '../components/Header.svelte'
-  // import { Scene } from '@zeainc/zea-engine'
 
   metatags.title = 'Zea Template Collab'
   metatags.description = 'Description coming soon...'
@@ -16,7 +15,7 @@
 <zea-layout orientation="vertical" cell-a-size="50" resize-cell-a="false" cell-b-size="100%" cell-c-size="0px" resize-cell-c="false">
   <div class="w-full slot-container" slot="a"><Header /></div>
   <zea-layout slot="b" cell-a-size="300px" cell-b-size="100%" cell-c-size="0px" resize-cell-c="false" >
-    <div class="slot-container" slot="a">Sidebar </div>
+    <div class="slot-container" slot="a">Sidebar</div>
     <div class="slot-container" slot="b">Renderer</div>
   </zea-layout>
 </zea-layout>
